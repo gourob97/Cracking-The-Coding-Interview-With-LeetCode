a8047b9b39846e999a4ecdd12c86547251b7de79
gourob97 <gourobmazumder507@gmail.com>
2021-08-17 09:52:00 +0600

    bruteforce

--- mergeKSortedList.py
+++ mergeKSortedList.py
@@ -5,23 +5,23 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        
+
         newList = []
-        
+
         for i in range(len(lists)):
             node = lists[i]
-            
+
             while node:
                 newList.append(node.val)
                 node = node.next
-            
-        
+
+
         newList.sort()
         #print(newList)
-        
+
         result = []
-        
-        
+
+
         for item in newList:
             result.append(ListNode(item))
         if len(result) == 0:
@@ -29,4 +29,3 @@ class Solution:
         for i in range(len(result)-1):
             result[i].next=result[i+1]
         return result[0]
-            
