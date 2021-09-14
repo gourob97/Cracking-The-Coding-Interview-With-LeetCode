952ca417affa4d8895694e25b123d36913136952
gourob97 <gourobmazumder507@gmail.com>
2021-09-15 00:15:40 +0600

    using nested loops, gives TLE. Got accepted by using set

---
+++ containsDuplicate.py
@@ -0,0 +1,13 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        myset=set()
+        
+        for i in range(len(nums)):
+            if nums[i] in myset:
+                return True
+            else:
+                myset.add(nums[i])
+        return False
+                
+        
+     
